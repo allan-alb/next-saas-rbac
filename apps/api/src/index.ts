2dec817d52aaa39cf7e4a1cf18877aa7bd43a31c
Allan <38442183+allan-alb@users.noreply.github.com>
2025-10-14 15:22:05 -0300

    chore(auth): improve types, add more subjects

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,11 +2,11 @@ import { defineAbilityFor } from '@saas/auth'
 
 const ability = defineAbilityFor({ role: 'ADMIN' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
+// const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCanDeleteUser = ability.can('delete', 'User')
 
 const userCannotDeleteUser = ability.cannot('delete', 'User')
 
-console.log(userCanInviteSomeoneElse)
+// console.log(userCanInviteSomeoneElse)
 console.log(userCanDeleteUser)
 console.log(userCannotDeleteUser)
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -4,12 +4,26 @@ import {
   type MongoAbility,
   AbilityBuilder,
 } from '@casl/ability'
+import { z } from 'zod'
+
 import type { User } from './models/user'
 import { permissions } from './permissions'
-import type { UserSubject } from './subjects/user'
-import type { ProjectSubject } from './subjects/project'
+import { userSubject } from './subjects/user'
+import { projectSubject } from './subjects/project'
+import { organizationSubject } from './subjects/organization'
+import { inviteSubject } from './subjects/invite'
+import { billingSubject } from './subjects/billing'
 
-type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+  z.tuple([z.literal('manage'), z.literal('all')]),
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
